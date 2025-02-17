065ffdf561863a2c40cb7a1126749cd69d5f13f3
Cass-Kennedy <alkennedy228@gmail.com>
2025-02-17 14:46:01 -0500

    Added comments for context.

--- CassKennedyCh2Project/CassKennedyCh2Project.cpp
+++ CassKennedyCh2Project/CassKennedyCh2Project.cpp
@@ -6,11 +6,14 @@ using namespace std;
 int main()
 {
 	float oceanRise = 1.5;
+	//This is how much the sea level rises each year.
 	int timeOne = 5;
 	int timeTwo = 7;
 	int timeThree = 10;
+	//The time values signifiy 5, 7, and 10 years, respectively.
 	cout << "The sea level will be " << timeOne * oceanRise << " millimeters higher in 5 years." << endl;
 	cout << "The sea level will be " << timeTwo * oceanRise << " millimeters higher in 7 years." << endl;
 	cout << "The sea level will be " << timeThree * oceanRise << " millimeters higher in 10 years." << endl;
+	//These calculate and display the raise in sea level at the three time values.
 	return 0;
 }
