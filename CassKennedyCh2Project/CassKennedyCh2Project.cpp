f1089d9c691b9252421454d6e32ba6e9f37cf7c0
Cass-Kennedy <alkennedy228@gmail.com>
2025-02-17 14:39:52 -0500

    Added extra context in the form of comments.

--- CassKennedyCh2Project/CassKennedyCh2Project.cpp
+++ CassKennedyCh2Project/CassKennedyCh2Project.cpp
@@ -1,5 +1,5 @@
 //This project is meant to calculate three separate values: future ocean levels, distance of a gas tank, and energy drink consumption.
-//This branch is meant to calculate energy drink consumption.
+//This branch is meant to calculate energy drink consumption and display the relevant information.
 
 #include <iostream>
 using namespace std;
@@ -8,6 +8,7 @@ int main()
 	int sampleSize = 16500;
 	float regularCustomerProportion = 0.15;
 	float citrusProportion = 0.58;
+	//Originally regularCustomerProportion and citrusProportion were percentages. I divided them by 100 to make them proportions and so that they were easier to use in calulations.
 	cout << "From the survey, approximately " << regularCustomerProportion*sampleSize  << " people purchase energy drinks regularly." << endl;
 	cout << "From the survey, approximately " << citrusProportion*sampleSize  << " people citrus flavored energy drinks." << endl;
 	cout << "This data is from a survey of " << sampleSize << " customers." << endl;
