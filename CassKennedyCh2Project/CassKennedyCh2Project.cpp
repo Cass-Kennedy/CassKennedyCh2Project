9ddfba9627daf074ef920c9ecb4bdce63d377cff
Cass-Kennedy <alkennedy228@gmail.com>
2025-02-17 14:41:43 -0500

    Added context through comments.

--- CassKennedyCh2Project/CassKennedyCh2Project.cpp
+++ CassKennedyCh2Project/CassKennedyCh2Project.cpp
@@ -1,13 +1,15 @@
 //This project is meant to calculate three separate values: future ocean levels, distance of a gas tank, and energy drink consumption.
-//This branch is meant to calculate distance of a gas tank.
+//This branch is meant to calculate distance of a gas tank and display the relevant information.
 
 #include <iostream>
 using namespace std;
 int main()
 {
 	int gasCapacity = 20;
+	//This is in gallons, i.e. the gas tank holds 20 gallons of gas.
 	float urbanMileage = 23.5;
 	float highwayMileage = 28.9;
+	//The mileage numbers (urban and highway) are in miles per gallon.
 	cout << "You can expect to travel " << gasCapacity * urbanMileage << " miles in town off of a full tank of gas." << endl;
 	cout << "You can expect to travel " << gasCapacity * highwayMileage << " miles on the highway off of a full tank of gas." << endl;
 	return 0;
