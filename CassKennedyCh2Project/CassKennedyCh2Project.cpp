5b129883671bb4e05539823193ef0bfa64516448
Cass-Kennedy <alkennedy228@gmail.com>
2025-02-17 14:06:56 -0500

    Made the first branch. It includes a header and the first function (calculating sea level rise).

--- CassKennedyCh2Project/CassKennedyCh2Project.cpp
+++ CassKennedyCh2Project/CassKennedyCh2Project.cpp
@@ -1,20 +1,16 @@
-// CassKennedyCh2Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//This project is meant to calculate three separate values: future ocean levels, distance of a gas tank, and energy drink consumption.
+//This branch is meant to calculate future ocean levels.
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	float oceanRise = 1.5;
+	int timeOne = 5;
+	int timeTwo = 7;
+	int timeThree = 10;
+	cout << "The sea level will be " << timeOne * oceanRise << " millimeters higher in 5 years." << endl;
+	cout << "The sea level will be " << timeTwo * oceanRise << " millimeters higher in 7 years." << endl;
+	cout << "The sea level will be " << timeThree * oceanRise << " millimeters higher in 10 years." << endl;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
