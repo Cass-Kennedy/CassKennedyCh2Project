90f30f074ca27984a7ec752c0199247d04624bb6
Cass-Kennedy <alkennedy228@gmail.com>
2025-02-17 14:35:04 -0500

    From the master branch, I changed the variables to the sample size and the proportions from the data. While these were phrased as percentages in the assignment, I divided them by 100 to make them proportions to make calculations easier. I also changed the text to contextualize the numbers.

--- CassKennedyCh2Project/CassKennedyCh2Project.cpp
+++ CassKennedyCh2Project/CassKennedyCh2Project.cpp
@@ -1,16 +1,15 @@
 //This project is meant to calculate three separate values: future ocean levels, distance of a gas tank, and energy drink consumption.
-//This branch is meant to calculate future ocean levels.
+//This branch is meant to calculate energy drink consumption.
 
 #include <iostream>
 using namespace std;
 int main()
 {
-	float oceanRise = 1.5;
-	int timeOne = 5;
-	int timeTwo = 7;
-	int timeThree = 10;
-	cout << "The sea level will be " << timeOne * oceanRise << " millimeters higher in 5 years." << endl;
-	cout << "The sea level will be " << timeTwo * oceanRise << " millimeters higher in 7 years." << endl;
-	cout << "The sea level will be " << timeThree * oceanRise << " millimeters higher in 10 years." << endl;
+	int sampleSize = 16500;
+	float regularCustomerProportion = 0.15;
+	float citrusProportion = 0.58;
+	cout << "From the survey, approximately " << regularCustomerProportion*sampleSize  << " people purchase energy drinks regularly." << endl;
+	cout << "From the survey, approximately " << citrusProportion*sampleSize  << " people citrus flavored energy drinks." << endl;
+	cout << "This data is from a survey of " << sampleSize << " customers." << endl;
 	return 0;
 }
