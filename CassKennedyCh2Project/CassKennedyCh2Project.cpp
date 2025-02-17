675501671adc9a941ef65f2285415820186132e7
Cass-Kennedy <alkennedy228@gmail.com>
2025-02-17 14:18:02 -0500

    Made the gas distance calculation. I removed the variables from the first branch and changed them to capacity of the tank and the gas mileage in towns and on the highway. I also changed the text to display these values in context.

--- CassKennedyCh2Project/CassKennedyCh2Project.cpp
+++ CassKennedyCh2Project/CassKennedyCh2Project.cpp
@@ -1,16 +1,14 @@
 //This project is meant to calculate three separate values: future ocean levels, distance of a gas tank, and energy drink consumption.
-//This branch is meant to calculate future ocean levels.
+//This branch is meant to calculate distance of a gas tank.
 
 #include <iostream>
 using namespace std;
 int main()
 {
-	float oceanRise = 1.5;
-	int timeOne = 5;
-	int timeTwo = 7;
-	int timeThree = 10;
-	cout << "The sea level will be " << timeOne * oceanRise << " millimeters higher in 5 years." << endl;
-	cout << "The sea level will be " << timeTwo * oceanRise << " millimeters higher in 7 years." << endl;
-	cout << "The sea level will be " << timeThree * oceanRise << " millimeters higher in 10 years." << endl;
+	int gasCapacity = 20;
+	float urbanMileage = 23.5;
+	float highwayMileage = 28.9;
+	cout << "You can expect to travel " << gasCapacity * urbanMileage << " miles in town off of a full tank of gas." << endl;
+	cout << "You can expect to travel " << gasCapacity * highwayMileage << " miles on the highway off of a full tank of gas." << endl;
 	return 0;
 }
